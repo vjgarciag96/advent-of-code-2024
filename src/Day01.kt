9878f8f2e40f46d129f15488a6f849191f399ca6
Víctor J. García <vjgarciaw@gmail.com>
2024-12-07 01:57:30 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,43 @@
+import java.util.regex.Pattern
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val separator = Pattern.compile("""\s+""")
+
+        val (leftLocations, rightLocations) = input.fold(
+            emptyList<Int>() to emptyList<Int>(),
+        ) { acc, line ->
+            val (leftLocation, rightLocation) = line.split(separator)
+            (acc.first + leftLocation.toInt()) to (acc.second + rightLocation.toInt())
+        }
+
+        return leftLocations.sorted().zip(rightLocations.sorted()) { leftLocation, rightLocation ->
+            abs(leftLocation - rightLocation)
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val separator = Pattern.compile("""\s+""")
+        val rightLocationCount = HashMap<Int, Int>()
+        val leftLocations = ArrayList<Int>()
+
+        input.forEach { line ->
+            val (leftLocation, rightLocation) = line.split(separator)
+            leftLocations.add(leftLocation.toInt())
+            rightLocationCount.merge(rightLocation.toInt(), 1, Int::plus)
+        }
+
+        return leftLocations.sumOf { location ->
+            val rightLocationCount = rightLocationCount[location] ?: 0
+            location * rightLocationCount
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
